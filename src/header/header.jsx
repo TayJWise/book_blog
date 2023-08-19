b8c009ca51fd4c13e30b90769e1dc0761bc8e9ac
TayJWise <taylorjwise1@gmail.com>
2023-08-19 11:14:05 -0500

    Changed img and adjusted height of  img

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <span className="headerTitleSm">React & Node</span>
             <span className="headerTitleLg">Blog</span>
            </div>
-           <img className="headerImg" src="https://images.pexels.com/photos/269583/pexels-photo-269583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+           <img className="headerImg" src="https://images.pexels.com/photos/2086622/pexels-photo-2086622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
         </div>
     )
 }
