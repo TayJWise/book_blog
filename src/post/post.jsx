b47c157f4b598ee5cc61ed5cfef957622c4f9151
TayJWise <taylorjwise1@gmail.com>
2023-08-19 21:25:04 -0500

    added post descriptions

--- src/post/post.jsx
+++ src/post/post.jsx
@@ -6,8 +6,8 @@ export default function Post() {
             <img className="postImg" src="https://images.pexels.com/photos/3007370/pexels-photo-3007370.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
             <div className="postInfo">
                 <div className="postGenres">
-                    <span className="postFantasy">Fantasy</span>
-                    <span className="postRomance">Romance</span>
+                    <span className="postGenre">Fantasy</span>
+                    <span className="postGenre">Romance</span>
                 </div>
                 <span className="postTitle">
                     Lorem ipsum dolor sit amet
@@ -15,6 +15,15 @@ export default function Post() {
                 <hr />
                 <span className="postDate">1 hour ago</span>
             </div>
+            <p className="postDesc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna 
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                qui officia deserunt mollit anim id est laborum.
+            </p>
         </div>
     )   
 }
