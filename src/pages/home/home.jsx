a01d17235d0ec93e0078e6a61c93f3404e7024d8
TayJWise <taylorjwise1@gmail.com>
2023-08-19 21:32:22 -0500

    moved folders into component folder

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
-import Header from "../../header/header"
-import Posts from "../../posts/posts.jsx"
-import Sidebar from "../../sidebar/sidebar"
+import Header from "../../components/header/header"
+import Posts from "../../components/posts/posts.jsx"
+import Sidebar from "../../components/sidebar/sidebar"
 import "./home.css"
 
 export default function HomePage() {
